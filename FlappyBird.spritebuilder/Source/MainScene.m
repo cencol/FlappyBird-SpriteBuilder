09d09322f6a408bd249d8e189c5b9c0614490630
ryan <ryan@ryans-iMac.local>
2014-11-24 12:17:19 -0500

    Automatically Committed for MakeGamesWithUs

--- FlappyBird.spritebuilder/Source/MainScene.m
+++ FlappyBird.spritebuilder/Source/MainScene.m
@@ -14,14 +14,14 @@
     CCNode *_ground2;
     NSArray *_grounds;
     
-    CCNode *_cloud1;
+/*    CCNode *_cloud1;
     CCNode *_cloud2;
     NSArray *_clouds;
     
     CCNode *_bush1;
     CCNode *_bush2;
     NSArray *_bushs;
-    
+ */
     NSTimeInterval _sinceTouch;
     
     NSMutableArray *_obstacles;
@@ -40,8 +40,8 @@
     self.userInteractionEnabled = TRUE;
     
     _grounds = @[_ground1, _ground2];
-    _bushs=@[_bush1,_bush2];
-    _clouds=@[_cloud1, _cloud2];
+ //   _bushs=@[_bush1,_bush2];
+//    _clouds=@[_cloud1, _cloud2];
     
     for (CCNode *ground in _grounds) {
         // set collision txpe
